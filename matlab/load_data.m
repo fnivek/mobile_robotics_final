2dda59ff105520d024d751880335afe0dc4d3e38
zheming zhou <zhezhou@umich.edu>
2018-03-22 20:21:41 -0400

    LOAD_DATA: Add model and scene data, add pl visulization, load function

--- matlab/load_data.m
+++ matlab/load_data.m
@@ -1,6 +1,22 @@
-function [source_pc, target_pc] = load_data(filename)
+function [source_pc, target_pc] = load_data(model,scene,plot)
     % Loads a source and target point cloud
     %   filename - file name of the data to load
-    source_pc = zeros(3, 1);
-    target_pc = zeros(3, 1);
+    source_pc_struct = load(model);
+    fn = fieldnames(source_pc_struct);
+    source_pc = getfield(source_pc_struct,fn{1});
+    
+    target_pc_struct = load(scene);
+    fn = fieldnames(target_pc_struct);
+    target_pc = getfield(target_pc_struct,fn{1}); 
+    
+    if(plot)
+        figure(1)
+        plot3(source_pc(1, :), source_pc(2, :), source_pc(3, :), 'k.');
+        title("Model point cloud")
+        axis equal
+        figure(2)
+        plot3(target_pc(1, :), target_pc(2, :), target_pc(3, :), 'k.');
+        title("Scene point cloud")
+        axis equal
+    end
 end
--- matlab/run.m
+++ matlab/run.m
@@ -1,14 +1,18 @@
-function run(data_file)
+function run(model,scene)
     % Runs the curvature ICP code
     %   data_file - The filename to load data from
 
     % Set some reasonable defaults
-    if ~exist('data_file', 'var')
-      data_file = 'data.mat';
+    plot_flag = true;
+    if ~exist('model', 'var')
+      model = 'data/model/toy_downsample.mat';
+    end
+    if ~exist('scene', 'var')
+      scene = 'data/scene/scene1_easy.mat';
     end
 
     % Load the data
-    [source_pc, target_pc] = load_data(data_file);
+    [source_pc, target_pc] = load_data(model,scene,plot_flag);
 
     % Run curvature ICP
     final_tf = curv_icp(source_pc, target_pc);
