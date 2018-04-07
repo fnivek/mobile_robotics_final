ac55dc723b78a324acfd20bfa12e187407602615
tonypan1995 <1045380922@qq.com>
2018-04-08 04:19:32 +0800

    Update is_converged.m

--- matlab/is_converged.m
+++ matlab/is_converged.m
@@ -6,15 +6,28 @@ function converged = is_converged(icp)
     %   converged - a bool representing if convergence has occurred or not
 
     % Persistent vars
-    persistent last_err;
+    global last_err;
+    global result;
+    global euc_dist;
 
     % Calculate error
-    err = 0;
+    src_cor = icp.correspondences(:,1);
+    tar_cor = icp.correspondences(:,2);
+    [n,~] = size(icp.correspondences);
+    dis = zeros(1,n);
+    for i=1:n
+        src_ind = src_cor(i);
+        tar_ind = tar_cor(i);
+        dis(i) = euc_dist(tar_ind, src_ind) ^ 2;
+    end
+    
+    err = sum(dis) / n;
     err_diff = abs(last_err - err);
 
     % Set convergence bool
-    converged = (icp.itter > icp.max_itters); % || err < icp.err_converged_eps || err_diff < icp.err_diff_converged_eps;
+    converged = (icp.itter > icp.max_itters) || (err < icp.err_converged_eps) || (err_diff < icp.err_diff_converged_eps);
 
     % Update last error
     last_err = err;
+    result.err = [result.err, err];
 end
