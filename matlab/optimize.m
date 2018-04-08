12fc11d3122b52d0f359f44267f7225b19cea90d
Justin Diep <34872109+jjdiep@users.noreply.github.com>
2018-04-08 11:52:50 -0400

    added error handling
    will take correspondences as individual input, else will assume is part of icp struct and will look for icp.correspondences

--- matlab/optimize.m
+++ matlab/optimize.m
@@ -1,19 +1,20 @@
-function opt_tf = optimize(icp)
+function opt_tf = optimize(icp,corr)
     % Find an optimal transform between source and target
     %   icp.correspondences - a n x 2 matrix where n is the number of points in source_pc and the first
     %   column is the index in source pc and the second column is the index in target pc
     %   icp.source_pc - the source_pc points
     %   icp.target_pc - the target_pc points
     %   opt_tf - is the resulting homogeneous transform produced by finding the optimal transformation
-    
+    if nargin < 2
+        corr = icp.correspondences;
+    end
     % make new source pt_cloud P and target pt_cloud based off of correspondences
-    % (might need to double check matrix dims)    
-    source_corr = icp.correspondences(:,1);
-    target_corr = icp.correspondences(:,2);
+    source_corr = corr(:,1);
+    target_corr = corr(:,2);
     P = icp.source_pc(:,source_corr);
     X = icp.target_pc(:,target_corr);
     
-    % From GetTransformation as part of Maani's SVD function (requires AA MATLAB package since it uses quaternion function)
+    % From GetTransformation as part of Maani's SVD function (requires AA MATLAB package)
     % Derived from class lecture on ICP
     % P and X are 3xN matrixes for N points in 3 dimensions, i.e. each column
     % corresponds to a single point. See Besl & McKay (1992) section III.C.
