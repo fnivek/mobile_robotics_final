764db66bb7b95d52c9fb0667b1e6aec8fb523d33
Justin Diep <34872109+jjdiep@users.noreply.github.com>
2018-04-12 21:55:32 -0400

    Part of sampling correspondences code change
    Modified to take in sample correspondences (and maps back to original source cloud correspondence indices)

--- matlab/ransac_optimize.m
+++ matlab/ransac_optimize.m
@@ -16,16 +16,23 @@ opt_tf = eye(4);
 besterr = inf;
 
 for i = 1:coeff.iterNum
-    % Random sample of correspondences
-    rsample = randperm(length(icp.correspondences),coeff.numPtsToSample);
-    sample_corr = icp.correspondences(rsample,:);
+    % Take random sample of correspondences
+    
+    % convert to original source cloud index
+    orig_corr(:,1) = icp.source_pc_sample_index(icp.correspondences(:,1));
+    orig_corr(:,2) = icp.correspondences(:,2);
+%     rsample = randperm(length(icp.correspondences),coeff.numPtsToSample);
+    rsample = randperm(length(orig_corr),coeff.numPtsToSample);
+%     sample_corr = icp.correspondences(rsample,:);
+    sample_corr = orig_corr(rsample,:);
 
     % Random sample transform of source point cloud
     sample_tf = optimize(icp,sample_corr);
     guess_pc = transform_pc(sample_tf,icp.source_pc);
 
     % Euclidean distance calculation
-    error_pc = (icp.target_pc(:,icp.correspondences(:,2)) - guess_pc(:,icp.correspondences(:,1))).^2;
+%     error_pc = (icp.target_pc(:,icp.correspondences(:,2)) - guess_pc(:,icp.correspondences(:,1))).^2;
+    error_pc = (icp.target_pc(:,orig_corr(:,2)) - guess_pc(:,orig_corr(:,1))).^2;
     error = sqrt(sum(error_pc,1));
 
     % Inlier threshold calculation
@@ -53,7 +60,8 @@ end
 % Probably remove, just for edge case if inliers not found
 if opt_tf == 0
     disp('this sucks')
-    opt_tf = optimize(icp,icp.correspondences);
+%     opt_tf = optimize(icp,icp.correspondences);
+    opt_tf = optimize(icp,orig_corr);
 end
 
 end
